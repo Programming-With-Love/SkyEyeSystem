a8cd3635e297f136d01dcb5babc4c4a8a1356717
杜少雄 <dushaoxiong@lixiang.com>
2023-06-15 15:08:50 +0800

    Revert "分词插件"
    This reverts commit 3c3a4b8b29f19283dde45eb06cb1fae00ef55cb8.

--- src/test/java/cn/shoxiongdu/SkyEyeSystem/HotSpotTest.java
+++ src/test/java/cn/shoxiongdu/SkyEyeSystem/HotSpotTest.java
@@ -1,9 +1,20 @@
 package cn.shoxiongdu.SkyEyeSystem;
 
+import cn.hutool.http.Header;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import cn.shoxiongdu.SkyEyeSystem.entity.hot.HotSpot;
+import cn.shoxiongdu.SkyEyeSystem.response.hotspot.CoderUtilBaseRes;
 import cn.shoxiongdu.SkyEyeSystem.task.hotspot.CrawlerTask;
+import cn.shoxiongdu.SkyEyeSystem.task.hotspot.HotDataCrawler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class HotSpotTest {
